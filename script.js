fe99f8de641c751c4bea7316af5bccfc7f75280d
Kolumbete <ekolumbet@gmail.com>
2025-03-13 18:15:03 +0300

    huge upd

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Сайт загружен!");
 
-    // Анимация ссылок при наведении
-    let links = document.querySelectorAll("nav ul li a");
-    links.forEach(link => {
-        link.addEventListener("mouseover", function() {
-            link.style.color = "#ff0";
-        });
-        link.addEventListener("mouseout", function() {
-            link.style.color = "";
-        });
-    });
-
-    // Плавное появление элементов при скролле
+    // Анимация при скролле
     let animatedElements = document.querySelectorAll(".fade-in");
 
     function checkScroll() {
